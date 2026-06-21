4023ab6e0e5515da73ee6e302af27e9ae4dce18c
void2610 <contact@void2610.dev>
2026-06-21 11:38:52 +0900

    feat: hittable_listクラスを実装

---
+++ libs/math/include/rtmath/hittable_list.hpp
@@ -0,0 +1,21 @@
+#include "hittable.hpp"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class hittable_list : public hittable {
+public:
+  hittable_list() {}
+  hittable_list(shared_ptr<hittable> object) { add(object); }
+
+  void clear() { objects.clear(); }
+  void add(shared_ptr<hittable> object) { objects.push_back(object); }
+
+  virtual bool hit(const ray &r, double tmin, double tmax,
+                   hit_record &rec) const;
+
+  std::vector<shared_ptr<hittable>> objects;
+};
