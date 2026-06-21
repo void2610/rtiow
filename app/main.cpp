f02c0d7411b15d15780b65f95e3db9a59b57f90a
void2610 <contact@void2610.dev>
2026-06-21 17:51:48 +0900

    feat: 誘電体マテリアルと金属のfuzz機能を実装

--- app/main.cpp
+++ app/main.cpp
@@ -47,13 +47,15 @@ int main() {
 
   hittable_list world;
   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5,
-                                make_shared<lambertian>(color(0.7, 0.3, 0.3))));
+                                make_shared<lambertian>(color(0.1, 0.2, 0.5))));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100,
                                 make_shared<lambertian>(color(0.8, 0.8, 0.0))));
   world.add(make_shared<sphere>(point3(1, 0, -1), 0.5,
-                                make_shared<metal>(color(.8, .6, .2))));
+                                make_shared<metal>(color(0.8, 0.6, 0.2), 0.3)));
   world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5,
-                                make_shared<metal>(color(.8, .8, .8))));
+                                make_shared<dielectric>(1.5)));
+  world.add(make_shared<sphere>(point3(-1, 0, -1), 0.45,
+                                make_shared<dielectric>(1.5)));
 
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remainning: " << j << ' ' << std::flush;
