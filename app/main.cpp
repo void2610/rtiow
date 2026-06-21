4c24a5c9009b830799a9754e3e0ee335f8311f5f
void2610 <contact@void2610.dev>
2026-06-21 14:51:37 +0900

    feat: カメラクラスを実装してレイ生成を統一

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <rtcore/camera.hpp>
 #include <rtcore/hittable.hpp>
 #include <rtcore/hittable_list.hpp>
 #include <rtcore/sphere.hpp>
@@ -24,21 +25,12 @@ color ray_color(const ray &r, const hittable &world) {
 }
 
 int main() {
-  const auto aspect_ratio = 16.0 / 9.0;
   const int image_width = 384;
-  const int image_height = static_cast<int>(image_width / aspect_ratio);
+  const int image_height = static_cast<int>(image_width / (16.0 / 9.0));
 
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-  auto viewport_height = 2.0;
-  auto viewport_width = aspect_ratio * viewport_height;
-  auto focal_length = 1.0;
-
-  auto origin = point3(0, 0, 0);
-  auto horizontal = vec3(viewport_width, 0, 0);
-  auto vertical = vec3(0, viewport_height, 0);
-  auto lower_left_corner =
-      origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+  camera camera;
 
   hittable_list world;
   world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
@@ -49,7 +41,7 @@ int main() {
     for (int i = 0; i < image_width; ++i) {
       auto u = double(i) / (image_width - 1);
       auto v = double(j) / (image_height - 1);
-      ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+      auto r = camera.get_ray(u, v);
       color pixel_color = ray_color(r, world);
       write_color(std::cout, pixel_color);
     }
